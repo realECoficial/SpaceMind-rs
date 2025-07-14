622816d38816bb90d5be672a9ab8981b35442996
E.C <136939439+realECoficial@users.noreply.github.com>
2025-07-14 23:47:00 +0000

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use dioxus::prelude::*;
 //TODO hacer que el backend este en otro .rs!!!
 static CSS: Asset = asset!("/assets/main.css");
 
+
 #[derive(serde::Deserialize)]
 struct DogApi {
     message: String,
@@ -33,18 +34,26 @@ fn DogView() -> Element {
         reqwest::get("https://dog.ceo/api/breeds/image/random")
             .await
             .unwrap()
-            .json::<DogApi>()
-            .await
+            .json::<DogApi>() .await
             .unwrap()
             .message
     });    
+
     rsx! {
         div { id: "dogview",
             img { src: img_src.cloned().unwrap_or_default() }
         }
         div { id: "buttons",
-            button { onclick: move |_| img_src.restart(), id: "skip", "skip" }
-            button { onclick: move |_| img_src.restart(), id: "save", "save!" }
+            button {
+                id: "save",
+                onclick: move |_| async move {
+                    let current = img_src.cloned().unwrap();
+                    img_src.restart();
+                    _ = save_dog(current).await;
+                },
+
+                "save!"
+            }
         }
     }    
 }
@@ -55,8 +64,6 @@ struct TitleState(String);
 
 
 
-
-
 fn Title() -> Element {
     // Consume that type as a Context
     let title = use_context::<TitleState>();
@@ -64,5 +71,26 @@ fn Title() -> Element {
         h1 { "{title.0}" }
     }
 }
+
 //DESDE ACA PARA ABAJO ES BACKEND
+// on the client:
+
+#[server]
+async fn save_dog(image: String) -> Result<(), ServerFnError> {
+    use std::io::Write;
+
+    // Open the `dogs.txt` file in append-only mode, creating it if it doesn't exist;
+    let mut file = std::fs::OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open("dogs.txt")
+        .unwrap();
+
+    // And then write a newline to it with the image url
+    file.write_fmt(format_args!("{image}\n"));
+
+    Ok(())
+}
+
 
